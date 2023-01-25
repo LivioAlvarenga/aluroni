3bc2a3d1f298fa730cd0995860ae16762e12d7b9
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-25 13:53:29 -0300

    criando footer inicial

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+export default function Footer() {
+ return (
+   <footer>
+     <h2>Aluraflix</h2>
+     <p>Veja filmes a qualquer hora!</p>
+     <Logo/>
+   </footer>
+ )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,13 @@
-import { ReactComponent as Logo } from "../assets/logo.svg";
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Footer from "components/Footer";
 
 export default function home() {
   return (
     <main>
-      <nav className="px-64">
+      <nav className="px-64 py-5">
         <Logo />
       </nav>
+      <Footer />
     </main>
   );
 }
