db69599ff3d4f377f39ec0529ad2f319265f688d
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-26 11:49:57 -0300

    add padding horizontal padrão

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex h-[300px] items-center bg-header bg-cover bg-no-repeat px-64">
+    <header className="padding-horizontal flex items-center bg-header bg-cover bg-no-repeat">
       <div className="w-2/5 min-w-[400px] text-[5rem] text-white">
         A casa do código e da massa
       </div>
