bdcc781dc94f73271e9f99d8e17a95595585bec2
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-26 12:06:37 -0300

    adicionado cores ao tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,15 @@ module.exports = {
       backgroundImage: {
         header: "url('./src/assets/images/header.png')",
       },
+      colors: {
+        dark: "#242536",
+        darkGrey: "#92929d",
+        grey: "#e4e4e4",
+        black: "#4c4d5e",
+        lightGrey: "#f6f6f6",
+        blue: "#282b57",
+        red: "#d73b3b",
+      },
     },
   },
   plugins: [],
