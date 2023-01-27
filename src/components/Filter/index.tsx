3ca04879bedb97c7fe447df46a08b394bf7804cf
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-27 11:11:45 -0300

    add componente filter e adicionando estilo ao clicar

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import React from "react";
+import filterData from "./filterData.json";
+
+interface FilterDataProps {
+  label: string;
+  id: number;
+}
+
+interface FilterProps {
+  filter: number | null;
+  setFilter: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export default function Filter({ filter, setFilter }: FilterProps) {
+  function handleSelectFilter(option: FilterDataProps) {
+    if (filter === option.id) return setFilter(null);
+    return setFilter(option.id);
+  }
+
+  return (
+    <div className="my-5 flex flex-wrap gap-6">
+      {filterData.map((option) => (
+        <button
+          key={option.id}
+          onClick={() => handleSelectFilter(option)}
+          className={clsx(
+            "flex h-10 w-[150px] items-center justify-center rounded-md border-none bg-gray text-lg font-bold tracking-wider hover:cursor-pointer",
+            {
+              ["bg-blue text-white"]: filter === option.id,
+            }
+          )}
+        >
+          {option.label}
+        </button>
+      ))}
+    </div>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,25 @@
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Filter from "components/Filter";
 import Header from "components/Header";
 import Search from "components/Search";
 import { useState } from "react";
 
 export default function home() {
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<number | null>(null);
 
   return (
     <main>
-      <nav className="py-5 paddingDefault">
+      <nav className="paddingDefault py-5">
         <Logo />
       </nav>
       <Header />
-      <section className="py-[50px] paddingDefault">
+      <section className="paddingDefault py-[50px]">
         <h2 className="mb-8 text-5xl text-dark">Cardápio</h2>
         <Search search={search} setSearch={setSearch} />
+        <div className="mb-8 flex w-full flex-wrap items-center justify-between">
+          <Filter filter={filter} setFilter={setFilter} />
+        </div>
       </section>
     </main>
   );
