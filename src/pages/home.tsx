fcc1b284f1b3c3ddc4000cd91711e7eb6dd8e618
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-31 15:23:57 -0300

    add responsividade em home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,21 +7,21 @@ export default function home() {
 
   return (
     <Wrapper>
-      <section>
+      <section className="flex flex-col">
         <h2 className="mb-8 text-5xl tracking-wider text-dark">
           Recomendações da cozinha
         </h2>
         <div className="flex flex-wrap justify-between gap-8 rounded-sm">
           {recommendedFood.map((food) => (
-            <div key={food.id}>
-              <div className="mb-3 w-[300px] overflow-hidden rounded-lg">
+            <div key={food.id} className="w-full desktop_md:w-auto">
+              <div className="mb-3 overflow-hidden rounded-lg desktop_md:w-[300px]">
                 <img
                   src={food.photo}
                   alt={food.title}
                   className="w-full rounded-lg duration-1000 ease-in-out hover:scale-125"
                 />
               </div>
-              <button className="w-full cursor-pointer rounded-md border-none bg-red py-3 text-xl font-bold tracking-wider text-white duration-500 ease-in-out hover:bg-redDark">
+              <button className="w-full cursor-pointer rounded-md border-none bg-red py-3 text-xl font-bold tracking-wider text-white duration-500 ease-in-out hover:bg-redDark desktop_md:w-[300px]">
                 Ver mais
               </button>
             </div>
