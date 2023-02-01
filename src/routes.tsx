7102335457e41aea0cadb257e594817e749740d5
Livio Alvarenga <livioalvarenga@gmail.com>
2023-02-01 14:29:45 -0300

    adicionando min 100vh no main

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import Footer from "components/Footer";
+import Header from "components/Header";
 import MenuNav from "components/MenuNav";
 import Wrapper from "components/Wrapper";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -7,20 +8,25 @@ import Menu from "./pages/menu";
 
 export default function AppRouter() {
   return (
-    <Router>
-      <MenuNav />
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cardapio" element={<Menu />} />
-        </Routes>
-      </Wrapper>
-      <Footer
-        backgroundColor={"bg-red"}
-        textColor={"text-white"}
-        iconColor={"fill-white"}
-        hoverIconColor={"group-hover:desktop_lg:fill-dark"}
-      />
-    </Router>
+    <main className="flex min-h-screen flex-col justify-between">
+      <Router>
+        <div>
+          <MenuNav />
+          <Header />
+        </div>
+        <Wrapper>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cardapio" element={<Menu />} />
+          </Routes>
+        </Wrapper>
+        <Footer
+          backgroundColor={"bg-red"}
+          textColor={"text-white"}
+          iconColor={"fill-white"}
+          hoverIconColor={"group-hover:desktop_lg:fill-dark"}
+        />
+      </Router>
+    </main>
   );
 }
